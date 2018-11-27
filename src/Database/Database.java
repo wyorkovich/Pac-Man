9dd0125d84a975e8d4acc65c6d05f328eb80dad5
Matt Erickson <matterickson9819@gmail.com>
2018-11-27 16:17:19 -0600

    Getting ready for Maven

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -3,7 +3,6 @@ package Database;
  * This class is the skeleton for the gson file that will act as our database
  * to store the high scores
  */
-import com.google.gson.*;
 
 public class Database {
 
