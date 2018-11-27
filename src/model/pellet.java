461a1416b05bb39aebb0c3c776624b9f875fad08
Matt Erickson <matterickson9819@gmail.com>
2018-11-27 15:47:58 -0600

    Added pellet class

---
+++ src/model/pellet.java
@@ -0,0 +1,24 @@
+package model;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class pellet {
+
+	private int reward;
+	private int radius;
+	private Color color = Color.YELLOW;
+	private Circle pellet;
+	
+	public pellet() {
+		reward = 100;
+		pellet.setCenterX(radius);
+		pellet.setCenterY(radius);
+		pellet.setFill(color);
+	}
+	
+	public Circle createPellet() {
+		return pellet;
+	}
+	
+}
